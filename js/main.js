7dd78140339587a12797aeb6e81d053ee8e6ec79
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2022-11-22 17:28:41 -0300

    Correção de bugs e implementação de mais coisas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 document.addEventListener("DOMContentLoaded",() =>{
     createSquares();
+
+    let guessedWords = [[]];
+
+    let availableSpace = 1;
+
+    let word = "dairy"
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    function getCurrentWordArr(){
+        const numberOfGuessedWords = guessedWords.length
+        return guessedWords[numberOfGuessedWords-1]
+    }    
+
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr()
+
+        if(currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+
+            const availableSpaceEl = document.getElementById(String(availableSpace))
+            availableSpace = availableSpace + 1
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+    function handleSubmitWord(){
+        const currentWordArr = getCurrentWordArr()
+        if(currentWordArr.length !== 5){
+            window.alert("Palavra precisa ter 5 letras")
+        }
+
+        const currentWord = currentWordArr.join("")
+
+        if(currentWord === word){
+            window.alert("Parabéns , você acertou")
+        }
+    }
+
     function createSquares(){
         const gameBoard = document.getElementById("board")
         for(let index = 0; index < 30 ; index++){
@@ -9,4 +49,18 @@ document.addEventListener("DOMContentLoaded",() =>{
             gameBoard.appendChild(square);
         }
     }
-})
+
+    for(let i = 0 ; i < keys.length ; i++){
+        keys[i].onclick = ({target}) => {
+            const letter = target.getAttribute("data-key");
+
+            if(letter === 'enter'){
+                handleSubmitWord()
+                return;
+            }
+
+            updateGuessedWords(letter)
+        };
+    }
+
+});
